e73d87aa88231dd1cd7ff73661b4fc7a02ae9ea7
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-13 16:47:45 -0300

    Switch to single quotes and fix `sumneko` config

--- lua/setup-plugins.lua
+++ lua/setup-plugins.lua
@@ -1,73 +1,74 @@
 require('lualine').setup()
 
-require("mason").setup({
+require('mason').setup({
   ui = {
     -- Whether to automatically check for new versions when opening the :Mason window.
     check_outdated_packages_on_open = true,
 
     -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
-    border = "double",
+    border = 'double',
 
     icons = {
       -- The list icon to use for installed packages.
-      package_installed = "✓",
+      package_installed = '✓',
       -- The list icon to use for packages that are installing, or queued for installation.
-      package_pending = "➜",
+      package_pending = '➜',
       -- The list icon to use for packages that are not installed.
-      package_uninstalled = "✗"
+      package_uninstalled = '✗'
     },
 
     keymaps = {
       -- Keymap to expand a package
-      toggle_package_expand = "<CR>",
+      toggle_package_expand = '<CR>',
       -- Keymap to install the package under the current cursor position
-      install_package = "i",
+      install_package = 'i',
       -- Keymap to reinstall/update the package under the current cursor position
-      update_package = "u",
+      update_package = 'u',
       -- Keymap to check for new version for the package under the current cursor position
-      check_package_version = "c",
+      check_package_version = 'c',
       -- Keymap to update all installed packages
-      update_all_packages = "U",
+      update_all_packages = 'U',
       -- Keymap to check which installed packages are outdated
-      check_outdated_packages = "C",
+      check_outdated_packages = 'C',
       -- Keymap to uninstall a package
-      uninstall_package = "X",
+      uninstall_package = 'X',
       -- Keymap to cancel a package installation
-      cancel_installation = "<C-c>",
+      cancel_installation = '<C-c>',
       -- Keymap to apply language filter
-      apply_language_filter = "<C-f>",
+      apply_language_filter = '<C-f>',
     },
   },
 })
 
-require("mason-lspconfig").setup()
+require('mason-lspconfig').setup()
 
 -- Gonna try automatic LSP server setup via mason first
 -- If you use this approach, make sure you don't also manually set up servers
 -- directly via `lspconfig` as this will cause servers to be set up more than
 -- once.
 
-require("mason-lspconfig").setup_handlers {
+require('mason-lspconfig').setup_handlers {
   -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
 
   function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {}
+    require('lspconfig')[server_name].setup {}
   end,
 
   -- Next, you can provide a dedicated handler for specific servers.
---  ["sumneko_lua"] = function ()
---    lspconfig.sumneko_lua.setup {
---      settings = {
---        Lua = {
---          diagnostics = {
---            globals = { "vim" }
---          }
---        }
---      }
---    }
---  end,
+  ['sumneko_lua'] = function ()
+    require('lspconfig').sumneko_lua.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { 'vim' }
+          }
+        }
+      }
+    }
+  end,
 
 }
 
