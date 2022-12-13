c136fe6c2f0f32246c745af60eb4412dee303023
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-13 16:44:46 -0300

    Add convenience variables and set <Leader> to <SPACE>

--- lua/general.lua
+++ lua/general.lua
@@ -1,58 +1,82 @@
+-- Convenience renaming just for clarity
+local set = vim.opt
+local setWindowOption = vim.wo
+local setGlobal = vim.g
+local vimscript = vim.cmd
+
+-- use space as a the leader key
+setGlobal.mapleader = ' '
+
+
 -- Indentation block
 -- use spaces when TAB is pressed
-vim.opt.expandtab = true
+set.expandtab = true
 spaces = 2
 -- number of spaces to show for a TAB character
-vim.opt.tabstop = spaces
+set.tabstop = spaces
 -- number of spaces for indent function (>>)
-vim.opt.shiftwidth = spaces
+set.shiftwidth = spaces
 -- number of spaces for a tab in editing operations
-vim.opt.softtabstop = spaces
+set.softtabstop = spaces
 -- Insert `tabstop` number of spaces when the `tab` key is pressed
-vim.opt.smarttab = true
-vim.opt.smartindent = true
+set.smarttab = true
+set.smartindent = true
 -- When shifting lines, round the indentation to the nearest
 -- multiple of `shiftwidth`
-vim.opt.shiftround = true
+set.shiftround = true
 -- New lines inherit the indentation of previous lines
-vim.opt.autoindent = true
+set.autoindent = true
 -- Indentation block ends
+
 
 -- Screen width
 -- 125 columns is good for git's pull request view
 -- but I like to work with vsplits, hence this width
-vim.opt.textwidth = 65
+set.textwidth = 65
 -- highlight next column after textwidth (vertical red bar to
 -- show visually when to stop writting or wrap manually some
 -- code)
--- TODO '+' gives error
---vim.opt.colorcolumn = +1
+-- TODO originally this was `colorcolumn =+1` to put the
+-- highlighted column after textwidth. However: 
+-- '+' gives error,
+-- fixed number doesn't get set,
+-- and `vim.wo.colorcolumn = vim.opt.textwidth + 1` gives
+-- "invalid value for option"
+-- `vim.wo.colorcolumn = vim.opt.textwidth.get()`
+-- `setWindowOption.colorcolumn:append(1)` gives
+-- attempt to call method 'append' (a nil value)
+--setWindowOption.colorcolumn:append(1)
+-- So I'm using VimScript
+vimscript [[
+  set colorcolumn=+1
+]]
 
 -- TODO it would be nice to have wrap on when on a comment and
 -- off when on code
-vim.opt.wrap = false
+set.wrap = false
 
 -- smartcase: used for the commands "/", "?", "n", "N", ":g" and ":s"
-vim.opt.smartcase = true
+set.smartcase = true
 
 -- number lines
-vim.opt.number = true
+set.number = true
 
 -- not scroll as I type, it's annoying
-vim.opt.incsearch = false
+set.incsearch = false
 
 -- don't wrap around file on searches
-vim.opt.wrapscan = false
+set.wrapscan = false
 
 -- show xterm title
-vim.opt.title = true
+set.title = true
 
 -- place only a single space between sentences when joinging lines.
-vim.opt.joinspaces = false
+set.joinspaces = false
 
 -- allow me to switch to another buffer even if I haven't written my changes
-vim.opt.hidden = true
+set.hidden = true
 
+-- TODO not working
 -- highlight current line
 -- Note: for some reason this doesn't work from here, but if you put it as
 -- ex command by hand, does work (WTF?!)
@@ -62,14 +86,14 @@ vim.cmd('hi CursorColumn cterm=NONE ctermbg=darkgrey ctermfg=white guibg=darkgre
 -- TODO next line gives error
 --vim.api.nvim_set_keymap('nvso', '<F6>' ':set cursorline!<enter>:set cursorline?<enter>')
 vim.api.nvim_set_keymap('n', '<Leader>c', ':set cursorcolumn!<enter>:set cursorcolumn?<enter>', { noremap = true })
-vim.opt.cursorline = true
+set.cursorline = true
 -- highlight current line the same way on any colorscheme
 vim.cmd('autocmd ColorScheme * highlight StatusLine ctermbg=darkgrey cterm=NONE guibg=darkgrey gui=NONE')
 
 
 -- more intuitive way to put the new split
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+set.splitbelow = true
+set.splitright = true
 
 -- highlight unwanted spaces:
 --   - no trailing spaces
@@ -81,7 +105,7 @@ vim.cmd('autocmd ColorScheme * highlight ExtraWhitespace ctermbg=red guibg=red')
 --vim.cmd('match ExtraWhitespace /\v\s+$|\s+\t+|\t+\s+|^\s+$/')
 
 -- style
-vim.opt.background = 'dark'
+set.background = 'dark'
 vim.cmd('colorscheme elflord')
 
 -- to last position we were editing on this file
@@ -97,7 +121,7 @@ vim.cmd('au BufRead * normal zR')
 
 -- So vim-gutter (and other based on this setting) reports faster
 -- default is 4000 (4 seconds) which is too much
-vim.opt.updatetime = 100
+set.updatetime = 100
 
 -- regexp: set very magic when doing global substitutions
 vim.api.nvim_set_keymap('c', '%s/', '%s/\v', { noremap = true })
