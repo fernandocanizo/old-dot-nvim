d57e9c43d34f40397da3fe9fb5a8d17553910cbd
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-12 22:28:06 -0300

    Add `mason`, `mason-lspconfig`, `nvim-lspconfig`
    Mason manages LSP configurations, DAP configurations. Also manages
    required packages needed to make stuff work.

--- lua/bootstrap-packer.lua
+++ lua/bootstrap-packer.lua
@@ -15,6 +15,7 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'https://github.com/wbthomason/packer.nvim'
 
+
   -- A better status line and in Lua
   -- better than: control, lightline, airline
   use 'https://github.com/nvim-lualine/lualine.nvim'
@@ -24,6 +25,19 @@ return require('packer').startup(function(use)
   -- 'https://github.com/nvim-lualine/lualine.nvim'
   -- requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   -- }
+
+
+  -- mason: Portable package manager for Neovim that runs everywhere
+  -- Neovim runs. Easily install and manage LSP servers, DAP servers,
+  -- linters, and formatters.
+  --
+  -- mason.nvim is optimized to load as little as possible during setup.
+  -- Lazy-loading the plugin, or somehow deferring the setup, is not
+  -- recommended.
+  use 'https://github.com/williamboman/mason.nvim'
+  use 'https://github.com/williamboman/mason-lspconfig.nvim'
+  use 'https://github.com/neovim/nvim-lspconfig'
+
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
