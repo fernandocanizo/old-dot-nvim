9ea9e40a9f9351dc889f2b8bddbb200c24a14531
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-14 16:16:17 -0300

    Add more mappings for Telescope functions

--- lua/telescope-mappings.lua
+++ lua/telescope-mappings.lua
@@ -1,8 +1,14 @@
 -- Mappings for telescope
 
+-- convenience renaming
 local tb = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', tb.find_files, {})
+vim.keymap.set('n', '<leader>ffg', tb.git_files, {})
 vim.keymap.set('n', '<leader>fg', tb.live_grep, {})
 vim.keymap.set('n', '<leader>fb', tb.buffers, {})
 vim.keymap.set('n', '<leader>fh', tb.help_tags, {})
+vim.keymap.set('n', '<leader>fc', tb.colorscheme, {})
+vim.keymap.set('n', '<leader>fo', tb.vim_options, {})
+vim.keymap.set('n', '<leader>fr', tb.registers, {})
+vim.keymap.set('n', '<leader>fk', tb.keymaps, {})
