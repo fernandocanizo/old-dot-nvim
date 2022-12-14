a1d5aada50ff92ce1943e12d15bdeebd6a571a94
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-14 20:14:48 -0300

    Add `airblade/vim-gitgutter` plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -62,6 +62,9 @@ return require('packer').startup(function(use)
     run = ':TSUpdate'
   }
 
+  use 'https://github.com/airblade/vim-gitgutter'
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
